a0f9447e5f96c8ffab7f729dfbe61f6b1e83682b
K-Joker <32004650+K-Joker@users.noreply.github.com>
2018-03-25 22:34:20 +0800

    参考代码

--- JokerTracer/Source/main.cpp
+++ JokerTracer/Source/main.cpp
@@ -14,10 +14,10 @@ const int HEIGHT = 600;
 
 
 
-Vector3 getColor(const Ray &ray, hitable_list& world, int depth)
+Vector3 getColor(const Ray &ray, hitable_list* world, int depth)
 {
 	hit_record hit;
-	if (world.hit(ray, 0,555555, hit))
+	if (world->hit(ray, 0,555555, hit))
 	{
 		//return 0.5 * Vector3(hit.normal.x + 1, hit.normal.y + 1, hit.normal.z + 1);
 
@@ -48,6 +48,60 @@ Vector3 getColor(const Ray &ray, hitable_list& world, int depth)
 
 }
 
+hitable_list * getWorld()
+{
+	/*Sphere *s1 = new Sphere(Vector3(0, 0, -1), 0.5f, new Lambertian(Vector3(0.8, 0.3, 0.3)));
+	Sphere *s2 = new Sphere(Vector3(0, -100.5, -1), 100, new Lambertian(Vector3(0.8, 0.8, 0.0)));
+	Sphere *s3 = new Sphere(Vector3(1, 0, -1), 0.5, new Metal(Vector3(0.8, 0.6, 0.2), 1.0));
+	Sphere *s4 = new Sphere(Vector3(-1, 0, -1), 0.5, new Dielectric(1.5));
+	Sphere *s5 = new Sphere(Vector3(-1, 0, -1), -0.45, new Dielectric(1.5));
+
+	std::vector<Sphere*> slist{ s1, s2, s3, s4 };*/
+
+
+	std::vector<Sphere*> slist;
+	Sphere *s1 = new Sphere(Vector3(0, -1000, 0), 1000.0f, new Lambertian(Vector3(0.5, 0.5, 0.5)));
+	slist.push_back(s1);
+	for (int a = -11; a < 11; a++)
+	{
+		for (int b = -11; b < 11; b++)
+		{
+			float choose_mat = drand48();
+			Vector3 center(a + 0.9 * drand48(), 0.2, b + 0.9 * drand48());
+			if ((center - Vector3(4,0.2,0)).legth() > 0.9)
+			{
+				s1 = NULL;
+				if (choose_mat < 0.8)
+				{
+					s1 = new Sphere(center, 0.2, new Lambertian(Vector3(drand48() * drand48(), drand48() * drand48(), drand48() * drand48())));
+				} 
+				else if (choose_mat < 0.95)
+				{
+					s1 = new Sphere(center, 0.2,
+						new Metal(
+							Vector3(0.5 * (1 + drand48()),0.5 * (1 + drand48()),0.5 * (1 + drand48())),
+							0.5 * drand48())
+						);		
+				}
+				else
+				{
+					s1 = new Sphere(center, 0.2, new Dielectric(1.5));
+				}
+				slist.push_back(s1);
+			}
+		}
+	}
+
+	s1 = new Sphere(Vector3(0, 1, 0), 1.0f, new Dielectric(1.5)); 
+	slist.push_back(s1);
+	s1 = new Sphere(Vector3(-4, 1, 0), 1.0f, new Lambertian(Vector3(0.4, 0.2, 0.1)));
+	slist.push_back(s1);
+	s1 = new Sphere(Vector3(4, 1, 0), 1.0f, new Metal(Vector3(0.7, 0.6, 0.5), 0.0));
+	slist.push_back(s1);
+
+	return new hitable_list(slist);
+}
+
 void setColor(int width, int height)
 {
 	Vector3 from = Vector3(-2, 2, 1);
@@ -56,16 +110,9 @@ void setColor(int width, int height)
 	float aperture = 0.2;
 	Camera cam(from, at, Vector3(0,1,0), 60, (float) width/ (float)height, aperture, dis_to_focus);
 
-	Sphere *s1 = new Sphere(Vector3(0, 0, -1), 0.5f, new Lambertian(Vector3(0.8,0.3,0.3)));
-	Sphere *s2 = new Sphere(Vector3(0, -100.5, -1), 100, new Lambertian(Vector3(0.8,0.8,0.0)));
-	Sphere *s3 = new Sphere(Vector3(1, 0, -1), 0.5, new Metal(Vector3(0.8, 0.6, 0.2), 1.0));
-	Sphere *s4 = new Sphere(Vector3(-1, 0, -1), 0.5, new Dielectric(1.5));
-	Sphere *s5 = new Sphere(Vector3(-1, 0, -1), -0.45, new Dielectric(1.5));
+	hitable_list *hitlist = getWorld();
 
-	std::vector<Sphere*> slist{ s1, s2, s3, s4 };
-	hitable_list hitlist(slist);
-
-	int samples = 20;
+	int samples = 1;
 
 	int * color = (int *)s_color;
 	for (int i = 0; i < width; i++)
